f870c220ec430dc8db3056575462e473e8cf0145
Cody Raskin <cody@antimatterhorn.com>
2025-05-31 12:48:27 -0700

    added glass for 3d

--- src/Generation/GlassNodeGenerator.py
+++ src/Generation/GlassNodeGenerator.py
@@ -2,6 +2,7 @@ from DataBase import *
 from LinearAlgebra import *
 from Trees import *
 from numpy import sqrt
+import random
 
 class GlassNodeGenerator2d:
     def __init__(self, numNodes):
@@ -10,17 +11,15 @@ class GlassNodeGenerator2d:
         self.bounds = [[-1, -1], [1, 1]]
         self.trials = 20*numNodes
         
-        pfield = FieldofVector2d("position")
-        minx = self.bounds[0][0]
-        maxx = self.bounds[1][0]
-        miny = self.bounds[0][1]
-        maxy = self.bounds[1][1]
+        pfield = FieldofVector2d("trials")
+        minx,miny = self.bounds[0]
+        maxx,maxy = self.bounds[1]
+        
         area = (maxx - minx) * (maxy - miny)
         mean_spacing = sqrt(area / numNodes)
         dmin = mean_spacing * 0.8
         radius = mean_spacing * 2.0
-        
-        import random
+
         x = random.uniform(self.bounds[0][0], self.bounds[1][0])
         y = random.uniform(self.bounds[0][1], self.bounds[1][1])
         pfield.addValue(Vector2d(x,y)) # initial seed point
@@ -43,3 +42,51 @@ class GlassNodeGenerator2d:
             pfield.addValue(Vector2d(next[0],next[1]))
             self.positions.append([next[0],next[1]])
 
+class GlassNodeGenerator3d:
+    def __init__(self, numNodes):
+        self.numNodes = numNodes
+        self.positions = []
+        self.bounds = [[-1, -1, -1], [1, 1, 1]]
+        self.trials = 20 * numNodes
+
+        pfield = FieldofVector3d("trials")
+        minx, miny, minz = self.bounds[0]
+        maxx, maxy, maxz = self.bounds[1]
+
+        volume = (maxx - minx) * (maxy - miny) * (maxz - minz)
+        mean_spacing = (volume / numNodes) ** (1.0 / 3.0)
+        dmin = mean_spacing * 0.8
+        radius = mean_spacing * 2.0
+
+        # Initial seed point
+        x = random.uniform(minx, maxx)
+        y = random.uniform(miny, maxy)
+        z = random.uniform(minz, maxz)
+        pfield.addValue(Vector3d(x, y, z))
+        self.positions.append([x, y, z])
+
+        for i in range(numNodes - 1):
+            tree = KDTree3d(pfield)
+            nbrs = numNodes
+            next = (0, 0, 0)
+            decay = (1.0 - i / numNodes)
+            local_radius = radius * decay**0.5
+
+            for j in range(self.trials):
+                x = random.uniform(minx, maxx)
+                y = random.uniform(miny, maxy)
+                z = random.uniform(minz, maxz)
+                trial = Vector3d(x, y, z)
+
+                # Reject points too close to existing ones
+                if tree.findNearestNeighbors(trial, dmin):
+                    continue
+
+                # Prefer points with fewest neighbors in a larger radius
+                tn_big = tree.findNearestNeighbors(trial, local_radius)
+                if len(tn_big) < nbrs:
+                    nbrs = len(tn_big)
+                    next = (x, y, z)
+
+            pfield.addValue(Vector3d(next[0], next[1], next[2]))
+            self.positions.append([next[0], next[1], next[2]])
