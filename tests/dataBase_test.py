b27466e150cdb6cf07fd56a87c94afc641638f2e
Cody Raskin <cody@antimatterhorn.com>
2025-07-08 11:25:58 -0700

    patching nodeList after compile to add the getter for fields

---
+++ tests/dataBase_test.py
@@ -0,0 +1,18 @@
+from yggdrasil import *
+from Physics import WaveEquation2d
+from Mesh import Grid2d
+
+nx = ny = 10
+constants = MKS()
+
+myNodeList = NodeList(nx*ny)
+
+grid = Grid2d(nx,ny,1,1)
+
+waveEqn = WaveEquation2d(nodeList=myNodeList,
+                            constants=constants,
+                            grid=grid,C=1)
+
+phi = myNodeList.phi
+
+print(phi)
