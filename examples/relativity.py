7188de8a2f60aefc67878ffe81e8515c6664ac73
Cody Raskin <cody@antimatterhorn.com>
2026-08-10 14:35:21 -0700

    making relativty example more user friendly

--- examples/relativity.py
+++ examples/relativity.py
@@ -1,9 +1,17 @@
 from yggdrasil import *
 from Calculators import TimeDilation
+from Colors import BRIGHT_CYAN, BRIGHT_GREEN, BRIGHT_YELLOW, BRIGHT_RED, colorize
 
-travelTime = 2 # years
-distance   = 4  # light-years
+if __name__ == "__main__":
+    commandLine = CommandLineArguments(travelTime=2, #years
+                                        distance=4   #light-years
+                                        )
 
-td = TimeDilation(travelTime,distance)
 
-print("To travel %3.2f light-years in %3.2f years, \nyou would need to travel %3.3f c \nand %3.3f years would pass on Earth"%(distance,travelTime,td.velocity,td.earthTime))
+    td = TimeDilation(travelTime,distance)
+
+    print("To travel",colorize("%3.2f light-years"%distance,BRIGHT_YELLOW),"in",
+        colorize("%3.2f years"%travelTime,BRIGHT_YELLOW),"(subective time),",
+        "\nyou would need to travel ",colorize("%3.3f c"%td.velocity,BRIGHT_RED),
+        "\nand",colorize("%3.3f years"%td.earthTime,BRIGHT_CYAN),
+        "would pass on Earth")
