e2f2d9c979b0d4ab0cd7c1c5205bb00757fddef8
Cody Raskin <cody@antimatterhorn.com>
2026-08-17 20:19:39 -0700

    visual test for fmm gravity

--- tests/Physics/fmmGravityTest.py
+++ tests/Physics/fmmGravityTest.py
@@ -1,57 +1,60 @@
 from yggdrasil import *
-from Physics import (NBodyGravity1d, FMMGravity1d, Kinematics1d,
-                      NBodyGravity2d, FMMGravity2d, Kinematics2d,
-                      NBodyGravity3d, FMMGravity3d, Kinematics3d)
-import Integrators
+from Animation import *
+from Physics import FMMGravity2d, Kinematics2d
+from RandomNodeGenerator import RandomNodeGenerator2d
 import numpy as np
 
-def buildNodeList(dim, numNodes, positions, masses, constants, Kinematics, gravityFactory):
-    nodeList = NodeList(numNodes)
-    kinematics = Kinematics(nodeList=nodeList, constants=constants)
-    gravity = gravityFactory(nodeList, constants)
-
-    VectorCtor = globals()[f"Vector{dim}d"]
-    posField = getattr(nodeList, f"getFieldVector{dim}d")("position")
-    velField = getattr(nodeList, f"getFieldVector{dim}d")("velocity")
-    massField = nodeList.getFieldDouble("mass")
-    for i in range(numNodes):
-        posField.setValue(i, VectorCtor(*positions[i]))
-        velField.setValue(i, VectorCtor(*([0.0] * dim)))
-        massField.setValue(i, masses[i])
-
-    integrator = getattr(Integrators, f"RungeKutta4Integrator{dim}d")(
-        packages=[kinematics, gravity], dtmin=1e-8, verbose=False)
-    controller = Controller(integrator=integrator, periodicWork=[], statStep=1, tstop=1e30)
-    controller.Step(nsteps=1)
-    # nodeList must outlive the returned Field -- it isn't kept alive by the Field itself.
-    return nodeList, getattr(nodeList, f"getFieldVector{dim}d")("acceleration")
-
-
-def checkDim(dim, NBody, FMM, Kinematics, numNodes=300, plummerLength=1e-6, tol=0.05):
-    rng = np.random.default_rng(3)
-    positions = rng.uniform(-10.0, 10.0, size=(numNodes, dim))
-    masses = rng.uniform(0.5, 2.0, size=numNodes)
-    constants = PhysicalConstants(1.0, 1.0, 1.0, 1.0, 1.0)
-
-    nlNBody, accNBody = buildNodeList(dim, numNodes, positions, masses, constants, Kinematics,
-        lambda nl, c: NBody(nodeList=nl, constants=c, plummerLength=plummerLength))
-    nlFMM, accFMM = buildNodeList(dim, numNodes, positions, masses, constants, Kinematics,
-        lambda nl, c: FMM(nodeList=nl, constants=c, plummerLength=plummerLength, maxSourcesPerLeaf=16))
-
-    diffSq = 0.0
-    exactSq = 0.0
-    for i in range(numNodes):
-        d = accFMM[i] - accNBody[i]
-        diffSq += d.mag2
-        exactSq += accNBody[i].mag2
-
-    l2RelErr = (diffSq / exactSq) ** 0.5
-    print(f"dim={dim}  L2 relative error (FMMGravity vs NBodyGravity): {l2RelErr:.3e}")
-    assert l2RelErr < tol, f"FMMGravity{dim}d diverged too far from NBodyGravity{dim}d: L2 rel err {l2RelErr:.3e}"
+class dumpState:
+    def __init__(self,nodeList,workCycle=1,G=1):
+        self.nodeList = nodeList
+        self.cycle = workCycle
+        self.dump = []
+        self.energy = []
+        self.G = G
+    def __call__(self,cycle,time,dt):
+        for i in range(self.nodeList.numNodes):
+            self.dump.append((self.nodeList.getFieldVector2d("position")[i].x,self.nodeList.getFieldVector2d("position")[i].y))
 
 
 if __name__ == "__main__":
-    checkDim(1, NBodyGravity1d, FMMGravity1d, Kinematics1d)
-    checkDim(2, NBodyGravity2d, FMMGravity2d, Kinematics2d)
-    checkDim(3, NBodyGravity3d, FMMGravity3d, Kinematics3d)
-    print("PASS")
+    bounds = np.asarray([[-1,-1],[1,1]])
+    vbounds = bounds * 0.015
+    numNodes = 20
+    posGenerator = RandomNodeGenerator2d(numNodes=numNodes,bounds=bounds)
+    velGenerator = RandomNodeGenerator2d(numNodes=numNodes,bounds=vbounds)
+    myNodeList = NodeList(numNodes)
+
+    constants = PhysicalConstants(6.378e+6,     # earth radius in m
+                                  5.972e+24,    # earth mass in kg 
+                                  1.0,          # s
+                                  1.0, 
+                                  1.0) 
+    loc = Vector2d(0, 0)
+
+    kinematics = Kinematics2d(nodeList=myNodeList, constants=constants)
+    nBodyGrav = FMMGravity2d(nodeList=myNodeList,
+                               constants=constants,
+                               plummerLength=0.01)
+    packages = [kinematics, nBodyGrav]
+
+    positions   = myNodeList.getFieldVector2d("position")
+    velocity    = myNodeList.getFieldVector2d("velocity")
+    mass        = myNodeList.getFieldDouble("mass")
+    for i in range(numNodes):
+        mass.setValue(i,10.0)
+        positions.setValue(i,Vector2d(posGenerator.positions[i][0],posGenerator.positions[i][1]))
+        velocity.setValue(i,Vector2d(velGenerator.positions[i][0],velGenerator.positions[i][1])*0.5)
+
+    integrator = RungeKutta4Integrator2d(packages=packages,
+                                         dtmin=0.1,verbose=False)
+  
+
+    dump = dumpState(myNodeList,workCycle=1000,G=constants.G)
+    periodicWork = [dump]
+
+    controller = Controller(integrator=integrator,periodicWork=[],statStep=1,tstop=1e3)
+
+    bounds = (-1,1,-1,1)
+
+
+    AnimateScatter(bounds, stepper=controller, positions=positions, frames=100, interval=50)
